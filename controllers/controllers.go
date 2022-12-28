f92d2f3892b1bad3596aefb07b8214620eb12c32
viniciusdsouza <vduartesantiago@gmail.com>
2022-12-28 14:20:38 -0300

    chore: set controllers functions

---
+++ controllers/controllers.go
@@ -0,0 +1,31 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func SignUp() gin.HandlerFunc {
+
+}
+
+func Login() gin.HandlerFunc {
+
+}
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPassword(userPassword, givenPassword string) (bool, string) {
+
+} 
+
+func ProductViewerAdmin() gin.HandlerFunc {
+
+}
+
+func SearchProduct() gin.HandlerFunc {
+
+}
+
+func SearchProductByQuery() gin.HandlerFunc {
+	
+}
